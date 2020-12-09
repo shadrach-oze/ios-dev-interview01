3b5b734371d6335e3b2b45034352ca9a76d20d8d
Shadrach Mensah <shadrach@daretoinnovate.com>
2020-12-09 00:41:43 +0000

    Initial Commit

--- DevChallenge/ViewController.swift
+++ DevChallenge/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+
+    let tableView = UITableView(frame: .zero, style: .insetGrouped)
+
+    var users:[(name:String,url:String)] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.frame = view.frame
+        view.addSubview(tableView)
+        tableView.backgroundColor = .systemGroupedBackground
+        tableView.dataSource = self
+        tableView.delegate = self
+        URLSession.shared.dataTask(with:URL(string: "https://jsonplaceholder.typicode.com/users")!) { data, _, _ in
+            let json = try! JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [[String:Any]]
+            json?.forEach({ item in
+                let name = item["name"] as! String
+                let web = item["website"] as! String
+                self.users.append((name,web))
+            })
+            self.tableView.reloadData()
+        }
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        users.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.textLabel?.text = users[indexPath.row].name
+        cell.detailTextLabel?.text = users[indexPath.row].url
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        UITableView.automaticDimension
+    }
 
 }
 
