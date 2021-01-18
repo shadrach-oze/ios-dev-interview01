d01c15b7f0ca0900b86b6e98716039276f719278
Mojisola Adebiyi <mojisolaadebiyi@gmail.com>
2021-01-18 17:22:15 +0100

    Refactor View Controller

--- DevChallenge/Main/ViewController.swift
+++ DevChallenge/Main/ViewController.swift
@@ -10,39 +10,40 @@ import UIKit
 class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
     let tableView = UITableView(frame: .zero, style: .insetGrouped)
-
-    var users:[(name:String,url:String)] = []
+    let cellReuseIdentifier = "cell"
+    let viewModel = UserViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableView()
+        setupViewModel()
+    }
+    private func setupTableView(){
         tableView.frame = view.frame
         view.addSubview(tableView)
         tableView.backgroundColor = .systemGroupedBackground
         tableView.dataSource = self
         tableView.delegate = self
-        URLSession.shared.dataTask(with:URL(string: "https://jsonplaceholder.typicode.com/users")!) { data, _, _ in
-            let json = try! JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [[String:Any]]
-            json?.forEach({ item in
-                let name = item["name"] as! String
-                let web = item["website"] as! String
-                self.users.append((name,web))
-            })
-            self.tableView.reloadData()
-        }
     }
+    
+    private func setupViewModel(){
+        viewModel.fetchCompletedDelegate = self
+        viewModel.fetchUserData()
+    }
+
 
     func numberOfSections(in tableView: UITableView) -> Int {
         1
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        users.count
+        viewModel.users.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
-        cell.textLabel?.text = users[indexPath.row].name
-        cell.detailTextLabel?.text = users[indexPath.row].url
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellReuseIdentifier)
+        cell.textLabel?.text = viewModel.users[indexPath.row].name
+        cell.detailTextLabel?.text = viewModel.users[indexPath.row].website
         cell.accessoryType = .disclosureIndicator
         return cell
     }
@@ -53,3 +54,15 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
 
 }
 
+
+extension ViewController: DataFetchProtocol {
+    func onFetchCompleted() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+    func onFetchFailed(error: Error) {
+        
+    }
+}
