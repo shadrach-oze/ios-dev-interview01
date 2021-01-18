aa8b0de22eda117c9762cf028999ed8205d0d157
Mojisola Adebiyi <mojisolaadebiyi@gmail.com>
2021-01-18 17:13:42 +0100

    Add Data fetch protocol Add User Model

---
+++ DevChallenge/Main/Protocols/DataFetchProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  DataFetchProtocol.swift
+//  DevChallenge
+//
+//  Created by Mojisola Adebiyi on 18/01/2021.
+//
+
+import Foundation
+
+protocol DataFetchProtocol {
+    func onFetchCompleted()
+    func onFetchFailed()
+}
---
+++ DevChallenge/Main/ViewModel/UserViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserViewModel.swift
+//  DevChallenge
+//
+//  Created by Mojisola Adebiyi on 18/01/2021.
+//
+
+import Foundation
